0f6fa5035fd27d2e886a29991aab445e6e8449a1
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-15 01:21:04 +0100

    Add files via upload

---
+++ namespaces.php
@@ -0,0 +1,11 @@
+<?php
+//Namespaces are qualifiers that solve two different problems:
+    //They allow for better organization by grouping classes that work together to perform a task
+    //They allow the same name to be used for more than one class
+
+//A namespace declaration must be the first thing in the PHP file.
+
+
+//More info and examples at https://www.w3schools.com/php/php_namespaces.asp
+  
+  ?>
